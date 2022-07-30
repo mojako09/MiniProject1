30970a7d5a00819f548ee37653cb1f565d73c59f
Matthew Ervin Silvestre <matt09silvestre@gmail.com>
2022-07-30 18:46:56 +0800

    edit

---
+++ react/my-app/src/SubmitButton.js
@@ -0,0 +1,6 @@
+function SubmitButton(props) {
+    return(
+      <button className="SubmitBtn"> {props.raul} {props.altname}</button>
+    );
+  }
+  export default SubmitButton;
---
+++ react/my-app/src/navbar.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+import navbar from './navbar';
+
+
+function App() {
+
+}
