f45d86dfa2fb7afc0c47525041e523551a8daf7c
Matthew Ervin Silvestre <matt09silvestre@gmail.com>
2022-09-23 12:39:56 +0800

    add

---
+++ react/New folder (2)/about.php
@@ -0,0 +1,112 @@
+<!-- Container for demo purpose -->
+<div class="container my-24 px-6 mx-auto">
+
+  <!-- Section: Design Block -->
+  <section class="mb-32 text-gray-800">
+    <div class="flex flex-wrap">
+      <div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-7/12 px-3 lg:px-6">
+        <h2 class="text-3xl font-bold mb-6">About the System</h2>
+        <p class="font-bold mb-2">Anim pariatur cliche reprehenderit?</p>
+        <p class="text-gray-500 mb-6">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+          autem numquam dolore molestias aperiam culpa alias veritatis
+          architecto eos, molestiae vitae ex eligendi libero eveniet
+          dolorem, doloremque rem aliquid perferendis.
+        </p>
+        <p class="text-gray-500 mb-6">
+          Distinctio corporis, iure facere ducimus quos consectetur ipsa ut
+          magnam autem doloremque ex! Id, sequi. Voluptatum magnam sed fugit
+          iusto minus et suscipit? Minima sunt at nulla tenetur, numquam
+          unde quod modi magnam ab deserunt ipsam sint aliquid dolores
+          libero repellendus cupiditate mollitia quidem dolorem odit
+        </p>
+          Praesentium voluptatibus temporibus consequatur non aspernatur?
+        </p>
+        <p class="text-gray-500 mb-6">
+          Minima sunt at nulla tenetur, numquam unde quod modi magnam ab
+          deserunt ipsam sint aliquid dolores libero repellendus cupiditate
+          mollitia quidem dolorem.
+        </p>
+      </div>
+      <div class="grow-0 shrink-0 basis-auto w-full md:w-5/12 px-3 lg:px-6">
+        <p class="font-bold mb-6">Any queries or concerns? Please feel free to message us and we are happy to help you!</p>
+        <form>
+          <div class="form-group mb-6">
+            <input type="text" class="form-control block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+              placeholder="Name">
+          </div>
+          <div class="form-group mb-6">
+            <input type="email" class="form-control block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+              placeholder="Email address">
+          </div>
+          <div class="form-group mb-6">
+            <textarea class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+            " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
+          </div>
+          <button type="submit" class="
+            w-full
+            px-6
+            py-2.5
+            bg-blue-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg
+            transition
+            duration-150
+            ease-in-out">Send</button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Design Block -->
+
+</div>
+<!-- Container for demo purpose -->
